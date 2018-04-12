7d5f1723dc1f345319eb321e833c306023ceea32
Joshua Yuan <joshua.yuano@gmail.com>
2018-04-11 21:37:05 -0700

    Finished with comments

--- Cube.java
+++ Cube.java
@@ -10,18 +10,18 @@ public class Cube extends RectangularPrism implements Shape{
 	public Cube(double len){
 		super(len, len, len);
 	}
-	
+	//returns dimensions of one side
 	public double getSize() {
 		return getLength();
 	}
-	
+	//returns surface area of cube
 	@Override
 	public double getSurfaceArea(){
 		return super.getSurfaceArea();
-		}
-
+	}
+	//returns volume of cube
 	@Override
 	public double getVolume(){
 		return super.getVolume();
-		}
+	}
 }
